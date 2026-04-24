6957bb81de05e2de21bad487a26104748c61ee5e
Alwaysnyzzz <alwaysnnyzz@gmail.com>
2026-04-25 05:50:24 +0700

    update api root message

--- api/index.js
+++ api/index.js
@@ -1,12 +1,6 @@
 module.exports = async function handler(req, res) {
   return res.status(200).json({
     status: true,
-    message: 'Api Hidup Silahkan Mulai Upload',
-    endpoints: {
-      image: '/api/uploader/image',
-      video: '/api/uploader/video',
-      audio: '/api/uploader/audio',
-      file: '/api/uploader/file'
-    }
+    message: 'Api CDN Aktif Silahkan Panduan Pemakaian Di api.nyzz.my.id'
   });
 };
